2b6ddb926e389124e9b4b37ee10f67251ff29ac9
mgcarbonell <mgcarbonell@gmail.com>
2021-12-30 16:27:20 -0800

    I will have to remember how to use sequelize another day, but for expidiency I'm switching to TypeORM.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import dotenv from "dotenv"
 import { app } from "./app"
 import logger from "./utils/Logger"
-import db from "./db/models/index"
 dotenv.config()
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 4000
 
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    logger.info(`Server started on port ${PORT}`)
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      logger.info(`Server started on port ${PORT}`)
+    })
+    logger.info(`Database connected`)
   })
-  logger.info(`Database connected`)
-})
+  .catch((err: Error) => {
+    logger.error(err)
+  })
