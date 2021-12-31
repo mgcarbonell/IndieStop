4ef39c344e2fcdea29c8020c864843ee86685467
mgcarbonell <mgcarbonell@gmail.com>
2021-12-30 17:20:44 -0800

    Finishes switching product from sequelize to TypeORM

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -13,4 +13,22 @@ import { CartItem } from "./cartitem"
 export class Product extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number
+
+  @Column("varchar", { nullable: false, length: 255 })
+  title!: string
+
+  @Column("numeric", { nullable: false, precision: 10, scale: 2 })
+  price!: number
+
+  @Column("text", { nullable: false })
+  description!: string
+
+  @Column("text", { nullable: false })
+  imgUrl!: string
+
+  @Column("smallint", { nullable: false })
+  stock!: number
+
+  @OneToMany(() => CartItem, (cartItem) => cartItem.product_id)
+  cartItems!: CartItem[]
 }
